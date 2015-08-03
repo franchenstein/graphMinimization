16d897ac3f1a595f2ec3d9106da2f05e9879ea77
Franch <dfranch6@gmail.com>
2015-08-02 23:25:59 -0300

    Created function which will do the statistical testing. Still thinking on how to adapt the graph parsing function.

--- probabilisticGraph.py
+++ probabilisticGraph.py
@@ -1,7 +1,19 @@
 import graph
 import obtainstat
+from scipy import stats
 
 class ProbabilisticGraph(Graph):
     def __init__(self, states, alphabet):
         Graph.__init__(self, states, alphabet)
+        
+    def compareMorphs(stateA, stateB, alpha, test):
+        morphA = [x[3] for x in stateA.outedges]
+        morphB = [x[3] for x in stateB.outedges]
+        if test == "chi-squared":
+            [X, p] = stats.chisquare(morphA, morphB)         
+        else if test == "ks":
+            [KS, p] = stats.ks_2samp(morphA, morphB)
+        return [p < alpha, p]   
+        
+        
         
