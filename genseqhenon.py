e99f9a05a4084bc717adbb4b817806ab63c5a779
Franch <dfranch6@gmail.com>
2016-03-02 23:40:52 -0300

    generate sequences files

---
+++ genseqhenon.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env
+import probabilisticGraph as pg
+g = pg.ProbabilisticGraph([],[])
+for L in range(4,12,2):
+    print L
+    g.parseGraphFile("./Resultados/graph_henon_generated_L"+str(L)+"_NoMoore.txt")
+    w = g.stateNamed("00")
+    s = g.generateSequence(10000000, w)
+    f1 = "./Resultados/sequence_henon_generated_L"+str(L)+"_10000000_NoMoore.txt"
+    f = open(f1,'w')
+    f.write(s)
+    f.close()   
+    g.parseGraphFile("./Resultados/graph_henon_generated_L"+str(L)+".txt")
+    w = g.stateNamed("00")
+    s = g.generateSequence(10000000, w)
+    f1 = "./Resultados/sequence_henon_generated_L"+str(L)+"_10000000.txt"
+    f = open(f1,'w')
+    f.write(s)
+    f.close()  
+g.parseGraphFile("./Resultados/graph_henon_crissis.txt")
+w = g.stateNamed("00")
+d = g.generateSequence(10000000, c.stateNamed("00"))
+d = d[:10000000]
+f = open("./Resultados/sequence_henon_crissis.txt", 'w')
+f.write(d)
+f.close()
